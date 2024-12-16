8b752b4c13a45bff73c136bd686ca9e8fcb5e995
Guanglei Liu <guanglei1025@gmail.com>
2024-12-15 20:17:04 -0600

    Initial Commit

---
+++ HatchAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HatchAssignment
+//
+//  Created by Guanglei Liu on 12/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HatchAssignment/HatchAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HatchAssignmentApp.swift
+//  HatchAssignment
+//
+//  Created by Guanglei Liu on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct HatchAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
